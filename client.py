45884540114c7df367634505cc94391515471021
Punvireakroth <vireakrothpun@gmail.com>
2022-07-18 14:05:11 +0700

    add Disconnect handling

--- client.py
+++ client.py
@@ -1,1 +1,30 @@
 # multi client connect to server 
+import socket
+
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.56.1"
+ADDR = (SERVER, PORT)
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+# After we connect with the server We need to send something to the server 
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello, World")
+send("You are great ")
+send("I believe in You")
+send(DISCONNECT_MESSAGE)
